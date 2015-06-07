a29b7e7bff55bf7a89d5e8434dc3a7f8c45b9f6e
mrgamer <mister.gamer@gmail.com>
2015-06-08 01:34:01 +0200

    evolving the project, gulpfile, less, gulp dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,29 @@
 'use strict';
 
-let gulp = require('gulp');
+let path = require('path');
 
-function defaultTask (done) {
-  console.log('hello world');
-  done();
+let // gulp deps
+  gulp = require('gulp'),
+  less = require('gulp-less');
+
+let // external deps
+  del = require('del');
+
+let destDir = 'build';
+
+function cleanDir (done) {
+  del(destDir, done);
 }
 
-gulp.task('default', defaultTask);
+function buildLess (done) {
+  return gulp.src('css/*.less').pipe(less({
+    paths: [
+      path.join(__dirname, 'node_modules', 'bootstrap/less'),
+      path.join(__dirname, 'css')
+    ]
+  })).pipe(gulp.dest(destDir));
+}
+
+gulp.task('clean', cleanDir);
+gulp.task('less', buildLess);
+gulp.task('default', gulp.series('clean', 'less'));
