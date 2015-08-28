55a02bb2132405ada5dab8175a68f65f3a6df943
mrgamer <mister.gamer@gmail.com>
2015-08-28 12:34:13 +0200

    I needed block-scope inside dev-proxy, so converted var to let

--- dev-proxy.js
+++ dev-proxy.js
@@ -1,24 +1,38 @@
-var // node modules
+'use strict';
+let // node modules
   httpProxy = require('http-proxy'),
   express = require('express'),
   debug = require('debug');
 
-var // node stdLib
+let // node stdLib
   fs = require('fs'),
   path = require('path'),
   url = require('url');
 
-var
+let
   server = express(),
   ad = httpProxy.createProxyServer({ changeOrigin: true }),
   dbgForward = debug('proxy:forward'),
   dbgServe = debug('proxy:serve');
 
 const baseDir = 'build';
-var dirs = ['fonts', 'libs', ''];
+let dirs = ['fonts', 'libs', ''];
 
 ad.on('proxyRes', function (proxyRes, req, res, options) {
   dbgForward(proxyRes.headers);
+
+  // location header fix www.alldebrid.com -> localhost:3000
+  if (proxyRes.headers.location) {
+    let location = proxyRes.headers.location;
+    if (!location.startsWith('http://www.alldebrid.com')) {
+      proxyRes.headers.location = 'http://localhost:3000/ad';
+      return;
+    }
+
+    let partialUrl = location.split('http://www.alldebrid.com')[1];
+    dbgForward('fixing header Location', partialUrl);
+    proxyRes.headers.location = 'http://localhost:3000/ad' + partialUrl;
+  }
   // proxyRes.setHeader('X-Special-Proxy-Header', 'foobar');
 });
 
@@ -27,9 +41,9 @@ ad.on('proxyRes', function (proxyRes, req, res, options) {
  * by this script.
  * The array itself is an URL form, ex: /style.css or /libs/angular.js
  */
-var localFiles = ['/'];
+let localFiles = ['/'];
 dirs.forEach(function (d) {
-  var dirListing = fs.readdirSync(path.join(__dirname, baseDir, d))
+  let dirListing = fs.readdirSync(path.join(__dirname, baseDir, d))
   .map(function (file) {
     return [d, file].join('/');
   });
@@ -40,14 +54,14 @@ localFiles = localFiles.map(function (file) {
 });
 
 server.use(function (req, res, next) {
-  var parsedUrl = url.parse(req.url);
-  var pathname = parsedUrl.pathname;
+  let parsedUrl = url.parse(req.url);
+  let pathname = parsedUrl.pathname;
   if (localFiles.indexOf(pathname) === -1) return next();
   dbgServe('Serving', pathname, 'by hand.');
   res.sendFile(pathname, { root: path.join(__dirname, 'build') });
 });
 
-server.use(function (req, res) {
+server.use('/ad', function (req, res) {
   dbgForward('Forwarding request:', req.url);
   ad.web(req, res, { target: 'http://www.alldebrid.com' });
 });
@@ -56,7 +70,7 @@ if (require.main === module) {
   server.set('port', process.env.PORT || 3000);
   server.set('hostname', process.env.HOSTNAME || '127.0.0.1');
 
-  var listen = server.listen(server.get('port'), server.get('hostname'), function () {
+  let listen = server.listen(server.get('port'), server.get('hostname'), function () {
     console.log('\x1b[36m%s\x1b[0m %s', 'Express server listening on:', 'http://' + listen.address().address + ':' + listen.address().port + '/');
   });
 }
