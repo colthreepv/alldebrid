141d1f981da716162d154728fd61fe0dd7a7c419
mrgamer <mister.gamer@gmail.com>
2015-08-30 16:59:30 +0200

    httpretry prototype, needs to be tested

--- src/service/ad-api.js
+++ src/service/ad-api.js
@@ -1,3 +1,39 @@
-module.exports = ['$http', function ($http) {
+var api = {
+  register: '/ad/register/'
+};
+var retryForever = true;
+var retryTimeout = 5000;
 
-}];
+exports = module.exports = function ($http, $q, $timeout) {
+
+  function httpAndRetry () {
+    var args = arguments;
+    var httpPromise = $http.apply(null, args);
+
+    // in case there is an error
+    httpPromise.error(function () {
+      if (!retryForever) return $q.resolve(arguments); // FIXME?
+      return $timeout(function () {
+        return httpAndRetry.apply(null, args);
+      }, retryTimeout);
+    });
+
+    return httpPromise;
+  }
+
+  this.checkLogin = function () {
+    return new $q(function (resolve) {
+      $http({
+        method: 'GET',
+        url: api.register,
+        responseType: 'document'
+      }).success(function (page) {
+        resolve(page);
+      });
+    });
+  };
+
+  this.httpAndRetry = httpAndRetry;
+
+};
+exports.$inject = ['$http', '$q', '$timeout'];
