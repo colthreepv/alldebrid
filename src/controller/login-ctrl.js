e19933adbf9b2dd5dfb4253056b3c13e19d61440
mrgamer <mister.gamer@gmail.com>
2015-09-08 23:14:52 +0200

    login controller rename to test 'as' syntax

---
+++ src/controller/login-ctrl.js
@@ -0,0 +1,30 @@
+exports = module.exports = function ($scope, $state, user) {
+  var ctrl = this;
+
+  // login functions
+  $scope.login = {};
+
+  $scope.loading = false;
+
+  this.username = null;
+  this.password = null;
+  this.loading = false;
+  this.loginFailed = false;
+
+  this.do = function () {
+    ctrl.loading = true;
+    user.login(ctrl.username, ctrl.password)
+    .then(function () {
+      $state.go('home');
+    })
+    .catch(function () {
+      ctrl.loginFailed = true;
+    })
+    .finally(function () {
+      ctrl.loading = false;
+    });
+  };
+
+};
+
+exports.$inject = ['$scope', '$state', 'user'];
