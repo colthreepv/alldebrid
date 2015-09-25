bcd37a5960d5c0b490f4c19ee8ba3e5bdb3d4779
colthreepv <mister.gamer@gmail.com>
2015-09-25 14:53:08 +0200

    torrent add state does not write in history, only as user experience improvement

--- src/main/config/_resolve.js
+++ src/main/config/_resolve.js
@@ -14,12 +14,12 @@ startTorrents.$inject = ['torrent'];
 function addMagnet ($stateParams, $state, $q, $timeout, torrent, user) {
   // in case user has already logged before
   if (user.status.logged) {
-    return torrent.add($stateParams.magnet).then($state.go.bind(null, 'home.torrents'));
+    return torrent.add($stateParams.magnet).then($state.go.bind(null, 'home.torrents', {}, { location: 'replace' }));
   }
 
   // otherwise logs in and then adds
   return user.isLogged().then(function () {
-    return torrent.add($stateParams.magnet).then($state.go.bind(null, 'home.torrents'));
+    return torrent.add($stateParams.magnet).then($state.go.bind(null, 'home.torrents', {}, { location: 'replace' }));
   }).catch(function () {
     $state.go('login', {
       goTo: 'home.add-external',
