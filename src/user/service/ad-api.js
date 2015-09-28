ec2c5b050882e9ee360b86e8b17c121a3e587ecd
mrgamer <mister.gamer@gmail.com>
2015-09-28 03:38:48 +0200

    first revision of lazy-ajax: it awaits a bit to communicate the ajax end status in case another ajax request starts soon after.
    In short: does not make your screen flicker with loading spinners.

--- src/user/service/ad-api.js
+++ src/user/service/ad-api.js
@@ -9,7 +9,10 @@ var api = {
 var retryForever = true;
 var retryTimeout = 5000;
 
+var ajaxDecayTime = 500; // ms
+
 exports = module.exports = function ($http, $q, $timeout, transformReq) {
+  var ajaxStatus = false;
   /**
    * httpRetry is a function to substitute to normal $http call
    * @return {Promise} -> $http response
@@ -121,15 +124,36 @@ exports = module.exports = function ($http, $q, $timeout, transformReq) {
     });
   };
 
+  /**
+   * Time 0: HTTP request starts
+   * Time 600: HTTP request completes
+   * Time 600: $timeout planned for Time 950 sends 'completed'
+   * Time 750: HTTP request starts - $timeout must be canceled
+   */
+
   // pub/sub to broadcast AJAX events through all application
   var ajaxListeners = [];
+  var ajaxTimeout, isCanceled;
   function sendAjax (newValue) {
+    isCanceled = $timeout.cancel(ajaxTimeout); // cancel the scheduled $timeout
+    if (isCanceled === true) { // means the user was getting a notification after 350ms saying 'completed', but instead a new HTTP occurred
+      if (newValue === false) ajaxTimeout = $timeout(notifyAjax.bind(null, newValue), ajaxDecayTime);
+      // if the new value is 'true', don't do anything, let the user wait for the 'false' notification
+    } else {
+      if (newValue === false) ajaxTimeout = $timeout(notifyAjax.bind(null, newValue), ajaxDecayTime);
+      else notifyAjax(newValue);
+    }
+  }
+  function notifyAjax (newValue) {
+    ajaxStatus = newValue;
     ajaxListeners.forEach(function (cb) {
       cb(newValue);
     });
   }
+
   this.onAjax = function (cb) {
-    ajaxListeners.push(cb);
+    ajaxListeners.push(cb); // add the listener to the queue
+    cb(ajaxStatus); // propagate actual status;
   };
 
   this.httpRetry = httpRetry;
