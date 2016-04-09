5f2c7d4c16ef914e06585f2e147af82ea968ecb2
colthreepv <mister.gamer@gmail.com>
2016-04-09 17:32:35 +0200

    homepage first commit

---
+++ backend/homepage.js
@@ -0,0 +1,54 @@
+'use strict';
+require('babel-register')({
+  babelrc: false,
+  presets: ['es2015', 'react'],
+  plugins: ['css-modules-transform'],
+  only: 'src'
+});
+const bundles = require('./config').bundles;
+const Promise = require('bluebird');
+
+// client deps
+const React = require('react');
+const createStore = require('redux').createStore;
+const Provider = require('react-redux').Provider;
+const renderToString = require('react-dom/server').renderToString;
+const App = require('../src/app').default;
+const reducers = require('../src/reducers').default;
+
+function homepage () {
+  const initialState = Promise.resolve(undefined); // empty state always for now
+  return initialState.then(templateHome);
+}
+
+module.exports = homepage;
+
+function templateHome (initialState) {
+  const store = createStore(reducers, initialState);
+  const html = renderToString(
+    React.createElement(Provider, { store },
+      React.createElement(App)
+    )
+  );
+
+  return `
+  <!doctype html>
+  <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>AllDebrid Better Frontend</title>
+    <meta name="description" content="">
+    <link rel="shortcut icon" type="image/x-icon" href="//cdn.alldebrid.com/lib/images/default/favicon.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <link rel="stylesheet" href="/<< rev['style.css'] >>"> -->
+  </head>
+  <body>
+    <div id="container">${html}</div>
+    <script>window.STATE_FROM_SERVER = ${JSON.stringify(initialState)}</script>
+    <script src="/build/${bundles.vendor}"></script>
+    <script src="/build/${bundles.js}"></script>
+  </body>
+  </html>`;
+}
+
