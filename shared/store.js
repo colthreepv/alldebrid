82aa29d5480d47c9737c59cc30ce446dc66a13ca
colthreepv <mister.gamer@gmail.com>
2016-05-19 00:14:24 +0200

    store supports redux debugger extension

--- shared/store.js
+++ shared/store.js
@@ -1,11 +1,16 @@
-import { createStore as create, applyMiddleware } from 'redux';
+import { createStore as create, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 function createStore (initialState, reducers) {
+  const middlewares = compose(
+    applyMiddleware(thunk),
+    typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
+  );
+
   return create(
     reducers,
     initialState,
-    applyMiddleware(thunk)
+    middlewares
   );
 }
 
