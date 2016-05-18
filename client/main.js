73e3dc7426f1610471635c0858834ccc8916a1c9
colthreepv <mister.gamer@gmail.com>
2016-05-19 01:08:33 +0200

    Main app client-side fixed

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import Login from '../shared/apps/login';
+import Main from '../shared/apps/main';
 import createStore from '../shared/store';
 import reducers from '../shared/apps/main/reducers';
 
@@ -10,7 +10,7 @@ const store = createStore(window.STATE_FROM_SERVER, reducers);
 
 render(
   <Provider store={store}>
-    <Login/>
+    <Main/>
   </Provider>,
   document.getElementById('container')
 );
