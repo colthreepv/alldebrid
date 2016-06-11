309bf6f373b8f9489e5d57eba7d0bc6f2928691d
colthreepv <mister.gamer@gmail.com>
2016-06-11 04:54:22 +0200

    ugly bug in redirectToLogin

--- backend/pages/index.js
+++ backend/pages/index.js
@@ -8,16 +8,19 @@ const err = {
   html: errorCodes.add(2000, 'requested an non-HTML page on a /pages/ endpoint')
 };
 
-const api = {
+const pages = {
   login: require('./login'),
   main: require('./main')
 };
 
 function html5fallback (req) { if (!req.accepts('html')) throw err.html().hc(404); }
-function redirectToLogin (req, res) { if (!auth.isValid(req.session)) return res.redirect('/login'); }
+function redirectToLogin (req, res, next) {
+  if (!auth.isValid(req.session)) return res.redirect('/login');
+  return next();
+}
 redirectToLogin['@raw'] = true;
 
 module.exports = {
-  login: promesso([html5fallback, api.login]),
-  main: promesso([html5fallback, redirectToLogin, api.main])
+  login: promesso([html5fallback, pages.login]),
+  main: promesso([html5fallback, redirectToLogin, pages.main])
 };
