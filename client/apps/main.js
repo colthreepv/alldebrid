56269373d6656e55bbf9ef3ad3b8297dd8624e0e
colthreepv <mister.gamer@gmail.com>
2016-06-11 04:54:22 +0200

    structured client/main but still empty, requires new API

--- client/apps/main.js
+++ client/apps/main.js
@@ -1,11 +1,33 @@
 import '../../css/index.scss';
 import angular from 'angular';
-import 'angular-ui-router';
+import uiRouter from 'angular-ui-router';
 
-const app = angular.module('main', ['ui.router']);
+import home from '../login/index';
+
+import http from '../shared/http';
+import api from '../shared/api';
+
+if (process.env.NODE_ENV === 'development') Error.stackTraceLimit = Infinity;
+const app = angular.module('main', [uiRouter]);
+
+app
+  .factory('http', http)
+  .factory('api', api)
+  .config(config)
+  .run(run);
+
+if (process.env.NODE_ENV === 'production') app.config(performance);
 
 function run () {
   console.log('angular-main is running');
 }
 
-app.run(run);
+function performance ($compileProvider) {
+  $compileProvider.debugInfoEnabled(false);
+}
+
+function config ($stateProvider, $locationProvider, $urlMatcherFactoryProvider) {
+  $locationProvider.html5Mode(true);
+  $urlMatcherFactoryProvider.strictMode(false);
+  $stateProvider.state(home);
+}
