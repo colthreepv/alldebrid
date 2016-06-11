41a497d0bb2b839a3a5d36f53bdb9bb81a73abf4
colthreepv <mister.gamer@gmail.com>
2016-06-11 04:54:22 +0200

    /api/unlock first commit

--- client/login/index.js
+++ client/login/index.js
@@ -1,3 +1,5 @@
+import angular from 'angular';
+
 function Controller (api, $location) {
   var $ctrl = this;
 
@@ -11,15 +13,17 @@ function Controller (api, $location) {
   this.loading = false;
   this.loginFailed = false;
   this.loginRecaptcha = false;
+  this.unlockToken = false;
 
   this.tryLogin = tryLogin;
+  this.tryUnlock = tryUnlock;
 
   this.loginData = {
     username: null,
     password: null
   };
 
-  this.fields = [
+  this.loginFields = [
     {
       key: 'username',
       type: 'input',
@@ -49,6 +53,28 @@ function Controller (api, $location) {
     }
   ];
 
+  this.unlockBaseInfo = null;
+  this.unlockData = {
+    unlock_token: null
+  };
+
+  this.unlockFields = [
+    {
+      key: 'unlock_token',
+      type: 'input',
+      wrapper: 'errors',
+      templateOptions: {
+        placeholder: 'Unlock Token',
+        required: true,
+        minlength: 6,
+        maxlength: 8,
+        addonLeft: {
+          class: 'glyphicon glyphicon-lock'
+        }
+      }
+    }
+  ];
+
   function tryLogin (form) {
     if (!form.$valid) return;
     $ctrl.loading = true;
@@ -56,14 +82,36 @@ function Controller (api, $location) {
     // in case login has been invoked with a return url, it gets triggered now!
     // FIXME parameters
     // if ($stateParams.goTo) $state.go($stateParams.goTo, $stateParams.params);
-    .then(() => $location.path('/'))
+    .then(response => {
+      if (response.status === 202) return unlockToken(response.data);
+      return redirect(response);
+    })
     .catch(err => err.status === 403 && recaptchaAppeared())
     .catch(() => this.loginFailed = true)
     .finally(() => this.loading = false);
   }
 
-  function recaptchaAppeared () {
-    $ctrl.loginRecaptcha = true;
+  function tryUnlock (form) {
+    if (!form.$valid) return;
+    const payload = angular.extend($ctrl.unlockBaseInfo, { unlock_token: $ctrl.unlockData.unlock_token });
+    $ctrl.loading = true;
+    return api.unlock(payload)
+    .then(redirect)
+    .catch(() => this.loginFailed = true)
+    .finally(() => this.loading = false);
+  }
+
+  function redirect (response) { $location.path(response.data.redirect); }
+  function recaptchaAppeared () { $ctrl.loginRecaptcha = true; }
+
+  function unlockToken (response) {
+    $ctrl.unlockToken = true;
+    $ctrl.unlockBaseInfo = {
+      username: $ctrl.loginData.username,
+      pepper: response.pepper,
+      geo_unlock: response.geo_unlock,
+      salt: response.salt
+    };
   }
 }
 Controller.$inject = ['api', '$location'];
