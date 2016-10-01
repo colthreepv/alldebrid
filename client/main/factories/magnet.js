58d186edaae620e40e9ea43dda69340bdaaffa6d
colthreepv <mister.gamer@gmail.com>
2016-10-01 11:42:01 +0200

    magnet register path had port even in HTTPS

--- client/main/factories/magnet.js
+++ client/main/factories/magnet.js
@@ -1,10 +1,11 @@
 /* @ngInject */
 function magnet ($window, $location, storage) {
-  var navigator = $window.navigator;
-  var port = $location.port() === 80 ? '' : ':' + $location.port();
-  var appUrl = $location.protocol() + '://' + $location.host() + port + '/add/%s';
-  var isWin10 = $window.navigator.userAgent.indexOf('Windows NT 10.0') !== -1;
-  var applicable = isWin10 && storage.get('win10-notification') === 'hide';
+  const navigator = $window.navigator;
+  const needsPort = !($location.port() === 80 || ($location.port() === 443 && $location.protocol() === 'https'));
+  const port = needsPort ? `:${$location.port()}` : '';
+  const appUrl = `${$location.protocol()}://${$location.host()}${port}/add/%s`;
+  const isWin10 = $window.navigator.userAgent.indexOf('Windows NT 10.0') !== -1;
+  const applicable = isWin10 && storage.get('win10-notification') === 'hide';
 
   return { boot, register };
 
